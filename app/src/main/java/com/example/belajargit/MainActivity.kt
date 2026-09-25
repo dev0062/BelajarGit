a4b668821fcbc23b68211d2fb93cfb0ecbb0773b
dev0062 <zaindevalldino@gmail.com>
2026-09-25 15:18:41 +0700

    Percobaan Commit 2

--- app/src/main/java/com/example/belajargit/MainActivity.kt
+++ app/src/main/java/com/example/belajargit/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BelajarGitTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Androidd",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
